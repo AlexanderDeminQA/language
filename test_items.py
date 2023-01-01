3ab93ca4d5f1b6e97d6b9157de519b98333c4776
schur <schurick.don@gmail.com>
2023-01-02 02:46:19 +0300

    New project items

---
+++ test_items.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import pytest
+
+link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
+
